2a9db99845e0c78abbca4f2c4446bd2a13d6c1fd
alperenkara <alprnkr@gmail.com>
2018-04-23 15:17:54 +0200

    second part

---
+++ ADS/LAB7/ak_lab7_ex_3.R
@@ -0,0 +1,29 @@
+#Advanced statistics-2018
+#Lab7
+#Alperen KARA
+setwd("~/AMSDC/AdvancedStatistics/LAB7")
+set.seed(1)
+
+###1 
+
+alp <- rnorm(100)
+
+###2 
+meanOfAlp <- mean(alp)
+# Answer 0.1088874
+sdOfAlp <- sd(alp)
+# Answer 0.8981994
+medianOfAlp <- median(alp)
+# Answer 0.1139092
+###3
+
+dec <- seq(0,1,0.1)
+quantile(alp,dec)
+
+###         0%         10%         20%         30%         40%         50%         60%         70%         80% 
+#-2.21469989 -1.05265747 -0.61386923 -0.37534202 -0.07670313  0.11390916  0.37707993  0.58121734  0.77125360 
+#90%        100% 
+#  1.18106508  2.40161776
+
+###$
+split(alp, ceiling(alp:4))
