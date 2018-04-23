a583b8c4a8c51a81178776877641293e3faf5f7d
alperenkara <alprnkr@gmail.com>
2018-04-23 15:25:38 +0200

    lastpart

--- ADS/LAB7/ak_lab7_ex_3.R
+++ ADS/LAB7/ak_lab7_ex_3.R
@@ -25,5 +25,28 @@ quantile(alp,dec)
 #90%        100% 
 #  1.18106508  2.40161776
 
-###$
+###4 Devide 
 split(alp, ceiling(alp:4))
+##5 Install
+install.packages("ISwR")
+library("ISwR", lib.loc="~/R/win-library/3.4")
+attach(juul)
+###
+### 6
+meanOfigf1<- mean(juul$igf1,na.rm=TRUE)
+
+#exercise 7
+countNumberONon <- sum(!is.na(juul$igf1))
+# 1018 occurences of a number
+
+#exercise 8
+descriptiveStatistics <- function(v){
+  #get rid of NA values
+  v<- na.omit(v)
+  m <- mean(v)
+  sdev <- sd(v)
+  va <- var(v)
+  med <- median(v)
+  sprintf("mean %f, standard deviation %f, variance %f, median %f", m,sdev,va,med)
+  
+}
