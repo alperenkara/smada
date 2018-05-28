94c6135e8a12a5d45b45dc9f7705948a4f82eff3
alperenkara <alprnkr@gmail.com>
2018-05-28 15:22:56 +0200

    ADS_LAB11_LastUpdate

--- ADS/LAB11/ak_lab11_exercises.R
+++ ADS/LAB11/ak_lab11_exercises.R
@@ -29,3 +29,27 @@ cor(a,c) # result : 0.7578741
 cor(b,c) # result : 0.7017614
 
 #EX 4
+#The highest correlation value is 0.7578741
+#for SO2 PM10
+
+model<- lm(a~c)
+model$coefficients[1] # 1.592554
+model$coefficients[2] # 0.1052081
+
+#EX 5
+
+plot(a~c)
+abline(model,col="red",lwd=1)
+
+#EX 6
+
+sum_of_the_squares_line<- function(y,x,z,t){
+  sum<-0
+  for(i in seq_along(y)){
+    sum<- sum + (y[i] - (z*x[i]+t))^2
+  }
+  sum
+}
+
+sum_of_the_squares_line(a,c,model$coefficients[2],model$coefficients[1])
+#3395.174
